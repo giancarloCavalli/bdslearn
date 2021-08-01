eff83a021a94d14e53210c89745e5bc3bc4a9650
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-01 18:36:26 -0300

    Section

--- backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/entities/Section.java
@@ -1,8 +1,6 @@
 package com.devsuperior.dslearnbds.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -10,16 +8,14 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
-import com.gcavalli.dscatalog.entities.enums.ResourceType;
-
 @Entity
-@Table(name = "resources")
-public class Resource implements Serializable {
+@Table(name = "sections")
+public class Section implements Serializable {
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
@@ -27,27 +23,28 @@ public class Resource implements Serializable {
 	private String description;
 	private String position;
 	private String imgUri;
-	private ResourceType type;
 	
 	@ManyToOne
-	@JoinColumn(name = "offer_id")
-	private Offer offer;
+	@JoinColumn(name = "resource_id")
+	private Resource resource;
 	
-	@OneToMany(mappedBy = "resource")
-	private List<Section> sections = new ArrayList<>();
+	@OneToOne
+	@JoinColumn(name = "prerequisite_id")
+	private Section prerequisite;
 	
-	public Resource() {
+	public Section() {
 	}
 
-	public Resource(Long id, String title, String description, String position, String imgUri, ResourceType type,
-			Offer offer) {
+	public Section(Long id, String title, String description, String position, String imgUri, Resource resource,
+			Section prerequisite) {
+		super();
 		this.id = id;
 		this.title = title;
 		this.description = description;
 		this.position = position;
 		this.imgUri = imgUri;
-		this.type = type;
-		this.offer = offer;
+		this.resource = resource;
+		this.prerequisite = prerequisite;
 	}
 
 	public Long getId() {
@@ -90,24 +87,20 @@ public class Resource implements Serializable {
 		this.imgUri = imgUri;
 	}
 
-	public ResourceType getType() {
-		return type;
+	public Resource getResource() {
+		return resource;
 	}
 
-	public void setType(ResourceType type) {
-		this.type = type;
+	public void setResource(Resource resource) {
+		this.resource = resource;
 	}
 
-	public Offer getOffer() {
-		return offer;
+	public Section getPrerequisite() {
+		return prerequisite;
 	}
 
-	public void setOffer(Offer offer) {
-		this.offer = offer;
-	}
-	
-	public List<Section> getSections() {
-		return sections;
+	public void setPrerequisite(Section prerequisite) {
+		this.prerequisite = prerequisite;
 	}
 
 	@Override
@@ -126,7 +119,7 @@ public class Resource implements Serializable {
 			return false;
 		if (getClass() != obj.getClass())
 			return false;
-		Resource other = (Resource) obj;
+		Section other = (Section) obj;
 		if (id == null) {
 			if (other.id != null)
 				return false;
--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -21,3 +21,7 @@ INSERT INTO offers (edition, start_Moment, end_Moment, course_id) VALUES ('2.0',
 INSERT INTO resources (title, description, position, img_Uri, type, offer_id) VALUES ('Trilha HTML', 'Trilha principal do curso', 1, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 1, 1);
 INSERT INTO resources (title, description, position, img_Uri, type, offer_id) VALUES ('Forum', 'Tire suas dúvidas', 2, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 2, 1);
 INSERT INTO resources (title, description, position, img_Uri, type, offer_id) VALUES ('Lives', 'Lives exclusivas para a turma', 3, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 0, 1);
+
+INSERT INTO sections (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 1', 'Neste capítulo vamos iniciar com tags básicas!', 1, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 1, null);
+INSERT INTO sections (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 2', 'Vamos nos aprofundar em tags de formulário!', 2, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 1, 1);
+INSERT INTO sections (title, description, position, img_Uri, resource_id, prerequisite_id) VALUES ('Capítulo 3', 'Vamos aprender sobre meta tags!', 3, 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 1, 2);
