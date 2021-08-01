ccfdb83343b14b3376f10c112cf780edcbebbc64
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-01 16:56:34 -0300

    Course, Offer, ORM, DB seed

--- backend/src/main/resources/data.sql
+++ backend/src/main/resources/data.sql
@@ -12,3 +12,8 @@ INSERT INTO user_roles (user_id, role_id) VALUES (2, 2);
 INSERT INTO user_roles (user_id, role_id) VALUES (3, 1);
 INSERT INTO user_roles (user_id, role_id) VALUES (3, 2);
 INSERT INTO user_roles (user_id, role_id) VALUES (3, 3);
+
+INSERT INTO courses (name, img_Uri, img_Gray_Uri) VALUES ('Bootcamp HTML', 'https://blog.coursify.me/wp-content/uploads/2018/08/plan-your-online-course.jpg', 'https://st4.depositphotos.com/7968596/25400/v/450/depositphotos_254003166-stock-illustration-online-course-isolated-icon-simple.jpg');
+
+INSERT INTO offers (edition, start_Moment, end_Moment, course_id) VALUES ('1.0', TIMESTAMP WITHOUT TIME ZONE '2020-11-20T03:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2021-11-20T03:00:00Z', 1);
+INSERT INTO offers (edition, start_Moment, end_Moment, course_id) VALUES ('2.0', TIMESTAMP WITHOUT TIME ZONE '2020-12-20T03:00:00Z', TIMESTAMP WITHOUT TIME ZONE '2021-12-20T03:00:00Z', 1);
