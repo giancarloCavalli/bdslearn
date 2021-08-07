792a2a9442266f5243f4619b37c20b05cc1cab0a
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-07 17:55:06 -0300

    Repositories, Exceptions, Validation, Security

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/resources/exceptions/ResourceExceptionHandler.java
@@ -0,0 +1,60 @@
+package com.devsuperior.dslearnbds.resources.exceptions;
+
+import java.time.Instant;
+
+import javax.servlet.http.HttpServletRequest;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.devsuperior.dslearnbds.services.exceptions.DatabaseException;
+import com.devsuperior.dslearnbds.services.exceptions.ResourceNotFoundException;
+
+@ControllerAdvice
+public class ResourceExceptionHandler {
+	
+	@ExceptionHandler(ResourceNotFoundException.class)
+	public ResponseEntity<StandardError> entityNotFound(ResourceNotFoundException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.NOT_FOUND;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Resource not found");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(DatabaseException.class)
+	public ResponseEntity<StandardError> entityNotFound(DatabaseException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.BAD_REQUEST;
+		StandardError error = new StandardError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Database exception");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		return ResponseEntity.status(status).body(error);
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<ValidationError> argumentNotValid(MethodArgumentNotValidException e, HttpServletRequest request) {
+		HttpStatus status = HttpStatus.UNPROCESSABLE_ENTITY;
+		ValidationError error = new ValidationError();
+		error.setTimestamp(Instant.now());
+		error.setStatus(status.value());
+		error.setError("Argument not valid exception");
+		error.setMessage(e.getMessage());
+		error.setPath(request.getRequestURI());
+		
+		e.getBindingResult().getFieldErrors().forEach(fe -> {
+			error.addError(fe.getField(), fe.getDefaultMessage());
+		});
+		
+		return ResponseEntity.status(status).body(error);
+	}
+	
+}
