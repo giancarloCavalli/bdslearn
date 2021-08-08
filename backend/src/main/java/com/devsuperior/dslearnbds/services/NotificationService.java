2377140db40e5530719c1a297b9395552b5d5343
Giancarlo Cavalli <gc.giancarloo@gmail.com>
2021-08-08 17:03:24 -0300

    currentUserNotifications

---
+++ backend/src/main/java/com/devsuperior/dslearnbds/services/NotificationService.java
@@ -0,0 +1,27 @@
+package com.devsuperior.dslearnbds.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.dslearnbds.dto.NotificationDTO;
+import com.devsuperior.dslearnbds.entities.User;
+import com.devsuperior.dslearnbds.repositories.NotificationRepository;
+
+@Service
+public class NotificationService {
+
+	@Autowired
+	private NotificationRepository repo;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional(readOnly = true)
+	public Page<NotificationDTO> currentUserNotifications(Pageable pageable) {
+		User user = authService.authenticated();
+		return repo.findByUser(user, pageable).map(x -> new NotificationDTO(x));
+	}
+}
